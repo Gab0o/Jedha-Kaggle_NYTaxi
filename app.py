189dd5d54fb0ad164decfbefcf49c0b0da4bcb31
Gabriel ROPERS <Gab00@Mac-de-Gab00.local>
2019-07-22 20:31:49 +0200

    app push

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, request
+from joblib import load
+import json
+​
+
+app = Flask(__name__)
+
+​
+@app.route('/predict', methods=['POST'])
+def predict():
+    values = json.loads(request.form['values'])
+    model = load('taxi_model')
+    prediction = model.predict(values)
+    return json.dumps(prediction.tolist())
+​
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
